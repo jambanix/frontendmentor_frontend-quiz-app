3d8cc606107420b7a833be7357c0769a8454ff05
jambanix <jambanix@pm.me>
2024-12-07 20:16:51 +0000

    set up tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "dark-navy": "#313e51",
+      "grey-navy": "#626c7f",
+      "light-bluish": "#abc1e1",
+      "light-grey": "#f4f6fa",
+      "green": "#26d782",
+      "navy": "#3b4d66",
+      "purple": "#a729f5",
+      "red": "#ee5454",
+      "pure-white": "#ffffff"
+    }
+  },
+  plugins: [],
+}
