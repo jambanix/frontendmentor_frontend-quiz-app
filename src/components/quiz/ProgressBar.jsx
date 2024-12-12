6aab5a765ee5eecaf6ca9fabf4413d631506c8f1
jambanix <jambanix@pm.me>
2024-12-12 18:33:32 +0000

    add progress bar colour by adding it into index.css as doesn't work with tailwind

--- src/components/quiz/ProgressBar.jsx
+++ src/components/quiz/ProgressBar.jsx
@@ -1,7 +1,7 @@
 export const ProgressBar = ({ currentQuestionNumber, maxQuestionNumber }) => {
   return (
     <div>
-      <progress value={currentQuestionNumber} max={maxQuestionNumber} className="bg-white w-full rounded-lg accent-red"/>
+      <progress value={currentQuestionNumber} max={maxQuestionNumber} className="bg-pure-white dark:bg-navy w-full rounded-lg progress"/>
     </div>
   );
 };
